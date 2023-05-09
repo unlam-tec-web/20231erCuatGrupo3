a752292aba80f38ebfe38f7b2844ec820b069cfb
Dario <darioorfano@hotmail.com>
2023-05-09 11:15:17 -0300

    Se agrego vista de login la cual incluye registro, se modifico problema en app.component.html, y se genero componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,49 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { CardComponent } from './components/card/card.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginViewComponent } from './views/login-view/login-view.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule,GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    LoginComponent,
     HomeViewComponent,
     CardComponent,
-    FooterComponent
+    FooterComponent,
+    LoginViewComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    SocialLoginModule,
+    GoogleSigninButtonModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '1000883229550-at62jqoeegh4eshinjhul0r5sdnanbh0.apps.googleusercontent.com'
+            )
+          },
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
