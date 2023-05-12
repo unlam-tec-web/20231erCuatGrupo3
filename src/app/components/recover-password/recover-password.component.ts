a2df4b8fd83cdb522ac2eb174cdbb19988ad8ed0
yamila <aragonyamila@gmail.com>
2023-05-12 11:07:14 -0300

    validacion de mail

--- src/app/components/recover-password/recover-password.component.ts
+++ src/app/components/recover-password/recover-password.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-recover-password',
   templateUrl: './recover-password.component.html',
   styleUrls: ['./recover-password.component.css']
 })
-/*hacer form*/
+
 export class RecoverPasswordComponent {
+
+  emailValidator  = new RegExp(/[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+
+  userEmail= new FormControl('',[Validators.required, Validators.email, Validators.pattern(this.emailValidator)]);
 
 }
