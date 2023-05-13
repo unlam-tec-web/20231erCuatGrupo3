e53c9ba0364c608e841b6f5d0ee0566f00299fa9
yamila <aragonyamila@gmail.com>
2023-05-12 23:09:08 -0300

    fixes

--- src/app/components/recover-password/recover-password.component.ts
+++ src/app/components/recover-password/recover-password.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-recover-password',
@@ -11,6 +11,8 @@ export class RecoverPasswordComponent {
 
   emailValidator  = new RegExp(/[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
 
-  userEmail= new FormControl('',[Validators.required, Validators.email, Validators.pattern(this.emailValidator)]);
+  constructor(private fb: FormBuilder) {}
 
+  recoverPassForm= this.fb.group({emailUser:['',[Validators.required,Validators.email,
+      Validators.pattern(this.emailValidator)]]})
 }
