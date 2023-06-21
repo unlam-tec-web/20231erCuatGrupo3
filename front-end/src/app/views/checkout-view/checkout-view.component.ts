fd7b38d93ed694bfcef1a83f413d72a774b8b84a
yamila <aragonyamila@gmail.com>
2023-06-21 17:40:36 -0300

    carrito dinamico en el checkout

--- front-end/src/app/views/checkout-view/checkout-view.component.ts
+++ front-end/src/app/views/checkout-view/checkout-view.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import {Cart} from 'src/assets/interfaces/cart.interface';
 @Component({
   selector: 'app-checkout-view',
   templateUrl: './checkout-view.component.html',
   styleUrls: ['./checkout-view.component.css']
 })
-export class CheckoutViewComponent {
+export class CheckoutViewComponent implements OnInit{
 
+  public cart: Cart []=[];
+
+  constructor(public cartService: CartService){}
+
+  ngOnInit(): void {
+    this.getCarrito();
+  }
+  public getCarrito(){
+    this.cart=this.cartService.getCart();
+  }
+
+  public getPrecio(producto: Cart){
+    this.cartService.obtenerPrecioPorCantidad(producto);
+  }
+  public getSubtotal(){
+    this.cartService.getSubtotal();
+  }
 }
