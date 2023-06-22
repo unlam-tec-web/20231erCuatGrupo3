7c7008e0235d8eb3d0e0b8a417a2919252266e89
yamila <aragonyamila@gmail.com>
2023-06-21 21:26:35 -0300

    arreglos

--- front-end/src/app/views/checkout-view/checkout-view.component.ts
+++ front-end/src/app/views/checkout-view/checkout-view.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
 import {Cart} from 'src/assets/interfaces/cart.interface';
+
 @Component({
   selector: 'app-checkout-view',
   templateUrl: './checkout-view.component.html',
@@ -9,20 +11,24 @@ import {Cart} from 'src/assets/interfaces/cart.interface';
 export class CheckoutViewComponent implements OnInit{
 
   public cart: Cart []=[];
+ //user
 
-  constructor(public cartService: CartService){}
+  constructor(public cartService: CartService, public userService: UserService){}
 
   ngOnInit(): void {
-    this.getCarrito();
-  }
-  public getCarrito(){
     this.cart=this.cartService.getCart();
   }
+ 
 
   public getPrecio(producto: Cart){
     this.cartService.obtenerPrecioPorCantidad(producto);
   }
   public getSubtotal(){
     this.cartService.getSubtotal();
+  }
+  public comprarProducto(){
+    //this.cartService.vaciarCarrito();
+    //this.cartService.comprarProducto(this.cart, this.user.id);
+    //redirigir a perfil y mostrar todos los pedidos
   }
 }
