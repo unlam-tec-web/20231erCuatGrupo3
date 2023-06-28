1be8869ccf10f91527fa8c829356dc11c59c3b41
CampanaJF <jucampana@alumno.unlam.edu.ar>
2023-06-28 14:14:36 -0300

    post cognito

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -7,7 +7,10 @@ import productController from "./controllers/productController";
 import categoryController from "./controllers/categoryController";
 import userController from "./controllers/userController";
 import {dbConnect} from "../config/database-config";
+import router from "./controllers/userController";
+const {userServices} = require("./services/userServices");
 dbConnect();
+
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -25,71 +28,63 @@ app.use("/api/products",productController);
 app.use("/api/categories",categoryController);
 app.use("/api/users",userController);
 
+app.post("api/users/login",async (req, res) => {
+    const{email, password} = req.body;
+    const result =await userServices.loginUser(email,password)
 
+    if (result.ok) {
+        res.status(200).send({
+            mensaje: result.mensaje,
+            usuario: result.usuario
+        });
+    } else{
+        res.status(401).send({
+            mensaje: result.mensaje,
+        });
 
-
-
-
-app.post("/api/users/login",async (req, res) => {
-  const{email, password} = req.body;
-  const result = await userServices.loginUser(email,password)
-  
-  if (result.ok) {
-    res.status(200).send({
-      mensaje: result.mensaje,
-      usuario: result.usuario
-    });
-  } else{
-    res.status(401).send({
-      mensaje: result.mensaje,
-    });
-    
-  }
+    }
 });
 
-app.post("/api/users/register", async (req, res) => {
-  const { name, apellido, direccion, emailRegistro, passwordRegistro } =
-    req.body;
-  const result = await userServices.registerUser(
-    name,
-    apellido,
-    direccion,
-    emailRegistro,
-    passwordRegistro
-  );
+app.post("api/users/register", async (req, res) => {
+    const { name, apellido, direccion, emailRegistro, passwordRegistro } =
+        req.body;
+    const result = await userServices.registerUser(
+        name,
+        apellido,
+        direccion,
+        emailRegistro,
+        passwordRegistro
+    );
 
-  if (result.ok) {
-    res.status(200).send({
-      mensaje:result.mensaje,
-    });
-  } else{
-    res.status(500).send({
-      mensaje: result.mensaje,
-    });
-    
-  }
+    if (result.ok) {
+        res.status(200).send({
+            mensaje:result.mensaje,
+        });
+    } else{
+        res.status(500).send({
+            mensaje: result.mensaje,
+        });
+
+    }
 });
 
+app.post('api/users/checkCode', async (req, res) => {
+    const email = req.body.email.trim();
+    const code = req.body.codigo;
+    const result = await userServices.verificarCodigo(email,code)
 
-app.post('/api/users/checkCode', async (req, res) => {
-  const email = req.body.email.trim();
-  const code = req.body.codigo;
-  const result = await userServices.verificarCodigo(email,code)
-  
-  if (result.ok) {
-    res.status(200).send({
-      mensaje:result.mensaje,
-    });
-  } else{
-    res.status(500).send({
-      mensaje: result.mensaje,
-    });
-    
-  }
+    if (result.ok) {
+        res.status(200).send({
+            mensaje:result.mensaje,
+        });
+    } else{
+        res.status(500).send({
+            mensaje: result.mensaje,
+        });
+
+    }
 
 });
-
-
 
 const port = 5000;
 app.listen(port, () => {
