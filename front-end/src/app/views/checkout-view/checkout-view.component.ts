3f2d5e16e08ac98a6ad3c1e6e1b2b0b366f7208e
CampanaJF <jucampana@alumno.unlam.edu.ar>
2023-06-28 15:14:43 -0300

    al finalizar la compra se vacia el carro

--- front-end/src/app/views/checkout-view/checkout-view.component.ts
+++ front-end/src/app/views/checkout-view/checkout-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart.service';
 import { UserService } from 'src/app/services/user.service';
 import {Cart} from 'src/assets/interfaces/cart.interface';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-checkout-view',
@@ -11,14 +12,21 @@ import {Cart} from 'src/assets/interfaces/cart.interface';
 export class CheckoutViewComponent implements OnInit{
 
   public cart: Cart []=[];
- //user
 
-  constructor(public cartService: CartService, public userService: UserService){}
+  constructor(
+    public cartService: CartService,
+    public userService: UserService,
+    protected router:Router,
+  ){}
 
   ngOnInit(): void {
     this.cart=this.cartService.getCart();
   }
- 
+
+  public confirmarCompra(){
+    this.cartService.vaciarCarrito();
+    this.router.navigate(['/order-confirmation']);
+  }
 
   public getPrecio(producto: Cart){
     this.cartService.obtenerPrecioPorCantidad(producto);
